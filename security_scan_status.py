49c658046c567125867eb0f120c9b88781bc4963
Davorin Špičko <davorin_spicko@yahoo.com>
2025-01-22 22:01:41 +0100

    [feature] Updated environment handler and Docker image build

---
+++ security_scan_status.py
@@ -0,0 +1,65 @@
+import requests
+import datetime
+
+class SecurityScanStatus():
+    '''
+    Class to store the status of a security scan
+
+    Attributes:
+        time (datetime): Time when status has been creted
+        host (str): Host of the website
+        path (str): Path of the website in CRX
+        url (str): URL of the website
+        response (requests.models.Response): Response object
+        status_code (int): Status code of the response
+        is_vulnerable (bool): True if a vulnerability was found, False otherwise
+    '''
+
+    def __init__(self, host, path, response):
+        '''
+        Constructor
+
+            Parameters:
+                host (str): Host of the website
+                path (str): Path of the website in CRX
+                response (requests.models.Response): Response
+        '''
+        
+        self.time = datetime.datetime.now()
+        self.host = host
+        self.path = path
+        self.url = '{host}{path}'.format(
+            host=self.host,
+            path=self.path
+        )
+        self.response = response
+        self.status_code = -1 if response is None else response.status_code
+        self.is_vulnerable = self.is_voulnerability_found(response)
+
+    def is_voulnerability_found(self, response):
+        '''
+        Check if a vulnerability was found in the response
+
+                Parameters:
+                    response (requests.models.Response): Response object
+
+                Returns:
+                    bool: True if a vulnerability was found, False otherwise
+        '''
+        if response is None:
+            return False
+        else:
+            return response.status_code != requests.codes.not_found
+
+    def __str__(self):
+        '''
+        Return string representation of the object
+
+                Returns:
+                    str: String representation of the object
+        '''
+        return '[{time}] {status_code} - {url}'.format(
+            time=self.time,
+            url=self.url,
+            status_code=self.status_code
+        )
---
+++ security_scanner.py
@@ -0,0 +1,174 @@
+import logging
+import requests
+import json
+import pathlib
+import threading
+
+from security_scan_status import SecurityScanStatus 
+
+VALID_FILE_EXTENSIONS = ['.txt', '.json']
+
+class SecurityScanner():
+    """Class to perfom an active security scan against a AEM dispatcher"""
+
+    results = []
+    counter = 0
+
+    def __init__(self, host = 'http://localhost:8080', page_path = '/content/geometrixx/en', request_timeout = 10, resource_path = 'aem-sec-paths.txt'):
+        '''
+        Constructor
+
+            Parameters:
+                host (str): Host of the website. Default is http://localhost:8080.
+                page_path (str): Path of the website in CRX. Default is /content/geometrixx/en.
+                request_timeout (int): Timeout for http requests in seconds. Default is 1.
+                resource_path (str): Path to the resource containing test patterns. Default is aem-sec-paths.txt.
+        '''
+        self.host = 'http://localhost:8080' if host is None else host
+        self.page_path = '/content/geometrixx/en' if page_path is None else page_path
+        self.request_timeout = 10 if request_timeout is None else request_timeout
+        self.paths = self.load_paths('aem-sec-paths.txt' if resource_path is None else resource_path)
+
+    def print_configuration(self):
+        """Prints configuration of SecurityScanner"""
+        logging.debug('Host is set to "{host}"'.format(
+            host=self.host))
+        logging.debug('Page path is set to "{page_path}"'.format(
+            page_path=self.page_path))
+        logging.debug('Request timeout is set to "{request_timeout}"'.format(
+            request_timeout=self.request_timeout))
+
+    def load_paths(self, resource_path):
+        '''
+        Loads path list from resource. Supported file extensions are .txt and .json.
+            
+                Parameters:
+                    resource_path (str): Path to the resource containing test patterns (can be local or remote)
+
+                Returns:
+                    list: List of paths
+        '''
+        if resource_path is None or resource_path == '':
+            logging.error('Resource path is not set')
+            return []
+        
+        extension = pathlib.Path(resource_path).suffix
+        if extension not in VALID_FILE_EXTENSIONS:
+            logging.error('Invalid file extension "{extension}". Valid file extensions are {valid_extensions}.'.format(
+                extension=extension, valid_extensions=VALID_FILE_EXTENSIONS))
+            return []
+
+        paths = []
+        if resource_path.startswith('http://') or resource_path.startswith('https://'):
+            r = requests.get(resource_path)
+            if r.status_code == requests.codes.ok:
+                paths = r.json() if resource_path.endswith('.json') else r.text.splitlines()
+            else:
+                logging.error('Failed to load resource from {resource_path}'.format(
+                    resource_path=resource_path))
+                paths = []
+        else:
+            with open(resource_path, 'r') as file:
+                paths = json.load(file) if resource_path.endswith('.json') else file.readlines()
+        
+        for i in range(len(paths)):
+            paths[i] = paths[i].strip()
+        return self.update_path_placeholders(paths)
+        
+
+    def update_path_placeholders(self, paths):
+        '''
+        Replaces placeholder '/content/add_valid_path_to_a_page' with valid website page path
+            
+                Parameters:
+                    paths (list): List of paths to be updated
+
+                Returns:
+                    list: List of updated paths
+        '''
+        updated_paths = []
+        for path in paths:
+            if path is not None and path != '':
+                p = path.replace("/content/add_valid_path_to_a_page", self.page_path)
+                updated_paths.append(p)
+        return updated_paths
+
+    def retrieve_path_response(self, path, headers = {}):
+        '''
+        Retrieve a response from the provided path
+            
+                Parameters:
+                    path (str): Path to retrieve response from
+                    headers (dict): Headers to be added to the request
+                
+                Returns:
+                    SecurityScanStatus: Status of the security scan
+                    
+        '''
+        url = '{host}{path}'.format(
+            host=self.host,
+            path=path
+        )
+        
+        try:
+            r = requests.get(
+                url,
+                headers=headers,
+                timeout=self.request_timeout)
+            
+            return SecurityScanStatus(self.host, path, r)
+        except requests.exceptions.RequestException as e:
+            logging.error('{error} for {url}'.format(error=e, url=url))
+        return None
+    
+    def retrieve_dispatcher_invalidate_cache_response(self):
+        '''
+        Retrieve a response distpacher invalidate cache endpoint
+
+                Returns:
+                    SecurityScanStatus: Status of the security
+                    
+        '''
+        headers = {
+            'CQ-Handle': '/content',
+            'CQ-Path': '/content'
+        }
+
+        return self.retrieve_path_response(path='/dispatcher/invalidate.cache', headers=headers)
+
+    def validate_all_paths(self):
+        '''
+        Performs vulnerability test for all paths provided to the scanner
+            
+                    Returns:
+                        list: List of results for each path provided to the scanner
+        '''
+        results = []
+        count = 0
+        for path in self.paths:
+            print('\rScanning {count} of {total} paths.'.format(count=count, total=len(self.paths)), end='', flush=True)
+            count += 1
+            results.append(self.retrieve_path_response(path))
+        results.append(self.retrieve_dispatcher_invalidate_cache_response())
+        return results
+
+    def retrieve_path_response_async(self, path, lock):
+        self.results.append(self.retrieve_path_response(path))
+        lock.acquire()
+        self.counter += 1
+        print('\rScanning {count} of {total} paths.'.format(count=self.counter, total=len(self.paths)), end='', flush=True)
+        lock.release()
+
+    def validate_all_paths_async(self):
+        self.results = []
+        self.counter = 0
+
+        lock = threading.Lock()
+        threads = []
+        for path in self.paths:
+            t = threading.Thread(target=self.retrieve_path_response_async, args=(path, lock,))
+            threads.append(t)
+            t.start()
+        
+        for t in threads:
+            t.join()
