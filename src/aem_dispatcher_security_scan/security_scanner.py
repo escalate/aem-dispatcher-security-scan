0c24532b44265ebf469c85abb01342a3cc3a90b7
Felix	Boerner <ich@felix-boerner.de>
2025-03-03 21:14:23 +0100

    style: use black format

--- src/aem_dispatcher_security_scan/security_scanner.py
+++ src/aem_dispatcher_security_scan/security_scanner.py
@@ -6,16 +6,23 @@ import requests
 from loguru import logger
 from security_scan_status import SecurityScanStatus
 
-VALID_FILE_EXTENSIONS = ['.txt', '.json']
+VALID_FILE_EXTENSIONS = [".txt", ".json"]
 
-class SecurityScanner():
+
+class SecurityScanner:
     """Class to perfom an active security scan against a AEM dispatcher"""
 
     results = []
     counter = 0
 
-    def __init__(self, host = 'http://localhost:8080', page_path = '/content/geometrixx/en', request_timeout = 10, resource_path = 'aem-sec-paths.txt'):
-        '''
+    def __init__(
+        self,
+        host="http://localhost:8080",
+        page_path="/content/geometrixx/en",
+        request_timeout=10,
+        resource_path="aem-sec-paths.txt",
+    ):
+        """
         Constructor
 
             Parameters:
@@ -23,23 +30,28 @@ class SecurityScanner():
                 page_path (str): Path of the website in CRX. Default is /content/geometrixx/en.
                 request_timeout (int): Timeout for http requests in seconds. Default is 1.
                 resource_path (str): Path to the resource containing test patterns. Default is aem-sec-paths.txt.
-        '''
-        self.host = 'http://localhost:8080' if host is None else host
-        self.page_path = '/content/geometrixx/en' if page_path is None else page_path
+        """
+        self.host = "http://localhost:8080" if host is None else host
+        self.page_path = "/content/geometrixx/en" if page_path is None else page_path
         self.request_timeout = 10 if request_timeout is None else request_timeout
-        self.paths = self.load_paths('aem-sec-paths.txt' if resource_path is None else resource_path)
+        self.paths = self.load_paths(
+            "aem-sec-paths.txt" if resource_path is None else resource_path
+        )
 
     def print_configuration(self):
         """Prints configuration of SecurityScanner"""
-        logger.debug('Host is set to "{host}"'.format(
-            host=self.host))
-        logger.debug('Page path is set to "{page_path}"'.format(
-            page_path=self.page_path))
-        logger.debug('Request timeout is set to "{request_timeout}"'.format(
-            request_timeout=self.request_timeout))
+        logger.debug('Host is set to "{host}"'.format(host=self.host))
+        logger.debug(
+            'Page path is set to "{page_path}"'.format(page_path=self.page_path)
+        )
+        logger.debug(
+            'Request timeout is set to "{request_timeout}"'.format(
+                request_timeout=self.request_timeout
+            )
+        )
 
     def load_paths(self, resource_path):
-        '''
+        """
         Loads path list from resource. Supported file extensions are .txt and .json.
 
                 Parameters:
@@ -47,37 +59,48 @@ class SecurityScanner():
 
                 Returns:
                     list: List of paths
-        '''
-        if resource_path is None or resource_path == '':
-            logger.error('Resource path is not set')
+        """
+        if resource_path is None or resource_path == "":
+            logger.error("Resource path is not set")
             return []
 
         extension = pathlib.Path(resource_path).suffix
         if extension not in VALID_FILE_EXTENSIONS:
-            logger.error('Invalid file extension "{extension}". Valid file extensions are {valid_extensions}.'.format(
-                extension=extension, valid_extensions=VALID_FILE_EXTENSIONS))
+            logger.error(
+                'Invalid file extension "{extension}". Valid file extensions are {valid_extensions}.'.format(
+                    extension=extension, valid_extensions=VALID_FILE_EXTENSIONS
+                )
+            )
             return []
 
         paths = []
-        if resource_path.startswith('http://') or resource_path.startswith('https://'):
+        if resource_path.startswith("http://") or resource_path.startswith("https://"):
             r = requests.get(resource_path)
             if r.status_code == requests.codes.ok:
-                paths = r.json() if resource_path.endswith('.json') else r.text.splitlines()
+                paths = (
+                    r.json() if resource_path.endswith(".json") else r.text.splitlines()
+                )
             else:
-                logger.error('Failed to load resource from {resource_path}'.format(
-                    resource_path=resource_path))
+                logger.error(
+                    "Failed to load resource from {resource_path}".format(
+                        resource_path=resource_path
+                    )
+                )
                 paths = []
         else:
-            with open(resource_path, 'r') as file:
-                paths = json.load(file) if resource_path.endswith('.json') else file.readlines()
+            with open(resource_path, "r") as file:
+                paths = (
+                    json.load(file)
+                    if resource_path.endswith(".json")
+                    else file.readlines()
+                )
 
         for i in range(len(paths)):
             paths[i] = paths[i].strip()
         return self.update_path_placeholders(paths)
 
-
     def update_path_placeholders(self, paths):
-        '''
+        """
         Replaces placeholder '/content/add_valid_path_to_a_page' with valid website page path
 
                 Parameters:
@@ -85,16 +108,16 @@ class SecurityScanner():
 
                 Returns:
                     list: List of updated paths
-        '''
+        """
         updated_paths = []
         for path in paths:
-            if path is not None and path != '':
+            if path is not None and path != "":
                 p = path.replace("/content/add_valid_path_to_a_page", self.page_path)
                 updated_paths.append(p)
         return updated_paths
 
-    def retrieve_path_response(self, path, headers = {}):
-        '''
+    def retrieve_path_response(self, path, headers={}):
+        """
         Retrieve a response from the provided path
 
                 Parameters:
@@ -104,48 +127,47 @@ class SecurityScanner():
                 Returns:
                     SecurityScanStatus: Status of the security scan
 
-        '''
-        url = '{host}{path}'.format(
-            host=self.host,
-            path=path
-        )
+        """
+        url = "{host}{path}".format(host=self.host, path=path)
 
         try:
-            r = requests.get(
-                url,
-                headers=headers,
-                timeout=self.request_timeout)
+            r = requests.get(url, headers=headers, timeout=self.request_timeout)
 
             return SecurityScanStatus(self.host, path, r)
         except requests.exceptions.RequestException as e:
-            logger.error('{error} for {url}'.format(error=e, url=url))
+            logger.error("{error} for {url}".format(error=e, url=url))
         return None
 
     def retrieve_dispatcher_invalidate_cache_response(self):
-        '''
+        """
         Retrieve a response distpacher invalidate cache endpoint
 
                 Returns:
                     SecurityScanStatus: Status of the security
-        '''
-        headers = {
-            'CQ-Handle': '/content',
-            'CQ-Path': '/content'
-        }
+        """
+        headers = {"CQ-Handle": "/content", "CQ-Path": "/content"}
 
-        return self.retrieve_path_response(path='/dispatcher/invalidate.cache', headers=headers)
+        return self.retrieve_path_response(
+            path="/dispatcher/invalidate.cache", headers=headers
+        )
 
     def validate_all_paths(self):
-        '''
+        """
         Performs vulnerability test for all paths provided to the scanner
 
                     Returns:
                         list: List of results for each path provided to the scanner
-        '''
+        """
         results = []
         count = 0
         for path in self.paths:
-            print('\rScanning {count} of {total} paths.'.format(count=count, total=len(self.paths)), end='', flush=True)
+            print(
+                "\rScanning {count} of {total} paths.".format(
+                    count=count, total=len(self.paths)
+                ),
+                end="",
+                flush=True,
+            )
             count += 1
             results.append(self.retrieve_path_response(path))
         results.append(self.retrieve_dispatcher_invalidate_cache_response())
@@ -155,7 +177,13 @@ class SecurityScanner():
         self.results.append(self.retrieve_path_response(path))
         lock.acquire()
         self.counter += 1
-        print('\rScanning {count} of {total} paths.'.format(count=self.counter, total=len(self.paths)), end='', flush=True)
+        print(
+            "\rScanning {count} of {total} paths.".format(
+                count=self.counter, total=len(self.paths)
+            ),
+            end="",
+            flush=True,
+        )
         lock.release()
 
     def validate_all_paths_async(self):
@@ -165,7 +193,13 @@ class SecurityScanner():
         lock = threading.Lock()
         threads = []
         for path in self.paths:
-            t = threading.Thread(target=self.retrieve_path_response_async, args=(path, lock,))
+            t = threading.Thread(
+                target=self.retrieve_path_response_async,
+                args=(
+                    path,
+                    lock,
+                ),
+            )
             threads.append(t)
             t.start()
 
--- src/cli.py
+++ src/cli.py
@@ -9,25 +9,25 @@ from aem_dispatcher_security_scan.security_scanner import SecurityScanner
 
 @click.command()
 @click.option(
-    '--host',
+    "--host",
     required=True,
-    default='http://localhost:8080',
-    help='Set host of website. Leave empty to use default value: http://localhost:8080.',
+    default="http://localhost:8080",
+    help="Set host of website. Leave empty to use default value: http://localhost:8080.",
 )
 @click.option(
-    '--page-path',
-    default='/content/geometrixx/en',
-    help='Set path of website. Leave empty to use default value: /content/geometrixx/en.',
+    "--page-path",
+    default="/content/geometrixx/en",
+    help="Set path of website. Leave empty to use default value: /content/geometrixx/en.",
 )
 @click.option(
-    '--timeout',
+    "--timeout",
     default=10,
-    help='Set timeout for http requests in seconds. Leave emtpy to use default value: 10.',
+    help="Set timeout for http requests in seconds. Leave emtpy to use default value: 10.",
 )
 @click.option(
-    '--verbose',
+    "--verbose",
     is_flag=True,
-    help='Enable verbose logging output.',
+    help="Enable verbose logging output.",
 )
 def cli(
     host,
@@ -43,8 +43,9 @@ def cli(
     scanner = SecurityScanner(host=host, page_path=page_path, request_timeout=timeout)
 
     # Run security scan
-    logger.info('Start active security scan of URL {website}'.format(
-        website=scanner.host))
+    logger.info(
+        "Start active security scan of URL {website}".format(website=scanner.host)
+    )
 
     scanner.validate_all_paths_async()
     results = scanner.results
@@ -55,14 +56,20 @@ def cli(
 
     # Display results
     if total_vulnerable == 0:
-        logger.info('Summary: No security relevant AEM Dispatcher URLs found in {total} rules.'.format(
-            total=total_scans))
+        logger.info(
+            "Summary: No security relevant AEM Dispatcher URLs found in {total} rules.".format(
+                total=total_scans
+            )
+        )
         click.exit(0)
     else:
-        logger.error('Summary: Found {hit} of {total} security relevant AEM Dispatcher URLs.\n\nVulnerable results are: \n{vulnerable_results}'.format(
-            hit=total_vulnerable,
-            total=total_scans,
-            vulnerable_results='\n'.join([str(r) for r in vulnerable_results])))
+        logger.error(
+            "Summary: Found {hit} of {total} security relevant AEM Dispatcher URLs.\n\nVulnerable results are: \n{vulnerable_results}".format(
+                hit=total_vulnerable,
+                total=total_scans,
+                vulnerable_results="\n".join([str(r) for r in vulnerable_results]),
+            )
+        )
         click.exit(1)
 
 
